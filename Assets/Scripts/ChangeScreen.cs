7e5e656925b0904b4e651a27b24fcba714679504
JDZ-3 <jd.smith@ufl.edu>
2017-10-25 14:14:37 -0400

    Add mirrors and dashcam

---
+++ Assets/Scripts/ChangeScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScreen : MonoBehaviour {
+
+    private int index = 0;
+    private Material[] m_list;
+    private SteamVR_TrackedObject trackedObj;
+    private Valve.VR.EVRButtonId grip = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+	// Use this for initialization
+	void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        m_list = transform.gameObject.GetComponent<MeshRenderer>().materials;
+        //StartCoroutine(RunSwitch());
+	}
+	
+	// Update is called once per frame
+	void Update () { if (controller.GetPressDown(grip)) { SwtchScreen(); } }
+
+    private void SwtchScreen()
+    {
+        index = (index + 1) % m_list.Length;
+        transform.gameObject.GetComponent<MeshRenderer>().material = m_list[index];
+    }
+
+    IEnumerator RunSwitch()
+    {
+        yield return new WaitForSeconds(5);
+        SwtchScreen();
+    }
+}
---
+++ Assets/Scripts/FlipVeiw.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipVeiw : MonoBehaviour {
+
+    private Camera cam;
+
+    // Use this for initialization
+    void Start() { cam = GetComponent<Camera>(); }
+
+    // Update is called once per frame
+    void Update () { }
+
+    void OnPreCull()
+    {
+        cam.ResetWorldToCameraMatrix();
+        cam.ResetProjectionMatrix();
+        cam.projectionMatrix = cam.projectionMatrix * Matrix4x4.Scale(new Vector3(-1, 1, 1));
+    }
+}
