b2652d9de33509a02958bffe1fed4ca84f9a49cd
JDZ-3 <jd.smith@ufl.edu>
2017-10-27 15:09:30 -0400

    Made a Test Scene that merges all progress in Sprint 1

---
+++ Assets/DashMirror/Scripts/ChangeScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScreen : MonoBehaviour {
+
+    private int index = 0;
+    private Material[] m_list;
+    private SteamVR_TrackedController trackedCtrl;
+
+//    private SteamVR_Controller.Device Controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+	// Use this for initialization
+	//void Start () { m_list = GameObject.Find("Mesh671").GetComponent<MeshRenderer>().materials; }
+
+    void Update() { }
+
+    private void OnEnable()
+    {
+        trackedCtrl = GetComponent<SteamVR_TrackedController>();
+        trackedCtrl.TriggerClicked += TriggerHandler;
+        m_list = GameObject.Find("Mesh671").GetComponent<MeshRenderer>().materials;
+    }
+
+    private void OnDisable() { trackedCtrl.TriggerClicked -= TriggerHandler; }
+
+    private void TriggerHandler(object sender, ClickedEventArgs e) { SwitchScreen(); }
+
+    private void SwitchScreen()
+    {
+        Debug.Log("Trigger Clicked");
+        index = (index + 1) % m_list.Length;
+        GameObject.Find("Mesh671").GetComponent<MeshRenderer>().material = m_list[index];
+    }
+}
