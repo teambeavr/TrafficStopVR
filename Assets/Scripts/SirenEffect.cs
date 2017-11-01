7369caf6e87202a5788ff11eca36670c7431f829
workday366 <jamesyiop@gmail.com>
2017-11-01 16:01:18 -0400

    add siren light and audio, implement input in htc vive

---
+++ Assets/Scripts/SirenEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SirenEffect : MonoBehaviour {
+
+	public Light redLight;
+    public Light blueLight;
+    public int speed;
+
+	private Vector3 redTemp;
+	private Vector3 blueTemp;
+	
+	// Update is called once per frame
+	void Update () {
+		redTemp.y += speed * Time.deltaTime;
+		blueTemp.y -= speed * Time.deltaTime;
+
+        redLight.transform.eulerAngles = redTemp;
+		blueLight.transform.eulerAngles = blueTemp;
+	}
+}
---
+++ Assets/Scripts/WordsInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordsInput : MonoBehaviour {
+
+    // 1
+    private SteamVR_TrackedObject trackedObj;
+    // 2
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Controller.GetHairTriggerDown())
+        {
+            Debug.Log("controller input");
+            Input.GetMouseButtonDown(0);
+        }
+    }
+}
