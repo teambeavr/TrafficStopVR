97cc670e50cc0fac6dd815bc32fe9e456588f868
Isaac Wang <wangi@ufl.edu>
2017-11-22 14:06:34 -0500

    Add new scenarios + dialogue

---
+++ Assets/Scripts/DoOnEnable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoOnEnable : MonoBehaviour {
+    public float delayTime = 0;
+    public UnityEvent onEnable;
+
+    void OnEnable()
+    {
+        StartCoroutine(DoAfterTime());
+    }
+
+    IEnumerator DoAfterTime()
+    {
+        yield return new WaitForSeconds(delayTime);
+        onEnable.Invoke();
+    }
+}
---
+++ Assets/Scripts/ScenarioPicker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioPicker : MonoBehaviour {
+    private int scenario = -1;
+	
+	void Awake () {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        // Check if in initial scene or not
+        if (GameObject.FindObjectOfType<DialogueManager>() == null)
+        {
+            LoadOutsideDialogue();
+        }
+        else
+        {
+            LoadScenarioDialogue();
+        }
+    }
+
+    public void SetScenario(int scenario)
+    {
+        this.scenario = scenario;
+    }
+
+    public void LoadScenarioDialogue()
+    {
+        var dlg = GameObject.FindObjectOfType<DialogueManager>();
+        switch (scenario)
+        {
+            default:
+            case 1:
+                dlg.startingAct = GameObject.Find("[Stop]").GetComponent<DialogueAct>();
+                break;
+            case 2:
+                dlg.startingAct = GameObject.Find("[Turn]").GetComponent<DialogueAct>();
+                break;
+            case 3:
+                dlg.startingAct = GameObject.Find("[Reckless]").GetComponent<DialogueAct>();
+                break;
+        }
+    }
+
+    public void LoadOutsideDialogue()
+    {
+
+    }
+}
