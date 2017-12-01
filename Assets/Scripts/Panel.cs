6061edcc85f2f1da835c70aa495070f116160110
workday366 <jamesyiop@gmail.com>
2017-12-01 17:39:12 -0500

    modify the car direction and resize the laptop

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel : MonoBehaviour {
+
+    public GameObject MessagePanel;
+    public Text button;
+    int counter;
+
+	// Use this for initialization
+	void Start () {
+        Image img = GameObject.Find("Panel").GetComponent<Image>();
+        img.color = UnityEngine.Color.cyan;
+	}
+
+    public void showHidePanel()
+    {
+        counter++;
+        if (counter%2 == 1)
+        {
+            MessagePanel.gameObject.SetActive(false);
+            button.text = "show hint";
+        }
+        else
+        {
+            MessagePanel.gameObject.SetActive(true);
+            button.text = "hide hint";
+        }
+    }
+	
+}
