85bb94f375b9ddd1d37ad88a7f5fceed97eaa574
workday366 <jamesyiop@gmail.com>
2017-12-05 13:14:13 -0500

    Mirror fix

---
+++ Assets/Scripts/NewMirror.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMirror : MonoBehaviour {
+
+    public Transform MirrorCam;
+    public Transform PlayerCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CalcRotation();
+	}
+
+    public void CalcRotation()
+    {
+        Vector3 dir = (PlayerCam.position - transform.position).normalized;
+        Quaternion rot = Quaternion.LookRotation(dir);
+
+        rot.eulerAngles = transform.eulerAngles - rot.eulerAngles;
+
+        MirrorCam.localRotation = rot;
+    }
+}
